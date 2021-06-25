da5c4a25ea8baced42406b9863fe045bec6c58fb
Marushkevych <marushkevych@gmail.com>
2021-06-25 14:55:55 -0400

    Todos react app without redux

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,23 +1,49 @@
+export const StatusFilters = {
+  All: 'all',
+  Active: 'active',
+  Completed: 'completed',
+}
+
 const initialState = {
-  status: 'All',
-  colors: ['blue']
+  status: StatusFilters.All,
+  colors: [],
 }
 
 export default function filtersReducer(state = initialState, action) {
   switch (action.type) {
     case 'filters/statusFilterChanged': {
       return {
+        // Again, one less level of nesting to copy
         ...state,
-        status: action.payload
+        status: action.payload,
       }
     }
     case 'filters/colorFilterChanged': {
-      const colors = action.payload.changeType === 'added'
-                     ? [...state.colors, action.payload.color]
-                     : state.colors.filter(color => action.payload.color !== color)
-      return {
-        ...state,
-        colors
+      let { color, changeType } = action.payload
+      const { colors } = state
+
+      switch (changeType) {
+        case 'added': {
+          if (colors.includes(color)) {
+            // This color already is set as a filter. Don't change the state.
+            return state
+          }
+
+          return {
+            ...state,
+            colors: state.colors.concat(color),
+          }
+        }
+        case 'removed': {
+          return {
+            ...state,
+            colors: state.colors.filter(
+                (existingColor) => existingColor !== color
+            ),
+          }
+        }
+        default:
+          return state
       }
     }
     default:
