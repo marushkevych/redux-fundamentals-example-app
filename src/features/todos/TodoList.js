0c75716f1648cdd8011af4d698a2c416852a8124
Marushkevych <marushkevych@gmail.com>
2021-07-08 16:57:07 -0400

    implement filtering

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {includes, isEmpty} from 'ramda'
 import {useSelector, shallowEqual} from 'react-redux'
 import TodoListItem from './TodoListItem'
+import {StatusFilters} from '../filters/filtersSlice'
 
-const selectTodoIds = state => state.todos.map(todo => todo.id)
+const filterPredicate = (status, colors) => todo => {
+  const matchesStatus = status === StatusFilters.All ? true :
+                        status === StatusFilters.Active ? !todo.completed : todo.completed
+
+  const matchesColor = isEmpty(colors) || includes(todo.color, colors)
+
+  return matchesStatus && matchesColor;
+}
+
+const selectTodoIds = state => {
+  return state.todos
+      .filter(filterPredicate(state.filters.status, state.filters.colors))
+      .map(todo => todo.id)
+}
 
 const TodoList = () => {
   console.log('TodoList is rendered')
